114d07174f491386283f32e9c60c9a23aa3205d3
meloveayu <2513367550@qq.com>
2019-09-06 22:33:51 +0800

    two activities is linked

--- app/src/main/java/com/example/loginandcalculator/MainActivity.java
+++ app/src/main/java/com/example/loginandcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.loginandcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else {
                         Toast.makeText(MainActivity.this,R.string.getin,Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(MainActivity.this,Main2Activity.class);
+                        startActivity(i);
                     }
                 }
             }
